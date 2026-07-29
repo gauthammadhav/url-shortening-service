53ef147d5689876ef0710d099ca45d6d4ae0ac8a
Gautham Madhav <gauthammadhav777@gmail.com>
2026-07-29 11:52:44 +0530

    feat: build URL shortner API with SQLAlchemy and Alembic

--- app/models.py
+++ app/models.py
@@ -7,6 +7,8 @@ class URL(Base):
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
     original_url: Mapped[str] = mapped_column(String(2048), nullable=False)
     short_code: Mapped[str] = mapped_column(String(6), unique=True, nullable=False)
+    click_count: Mapped[int] = mapped_column(default=0, nullable=False,server_default='0')
+    
     
     
     
